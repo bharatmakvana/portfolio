0206ed82832e8fac26cddb8e38aa83461fc229c8
b1m1 <bsmakvana5@gmail.com>
2025-06-01 16:23:52 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+Portofolio React App
---
+++ src/assets/assets.tsx
@@ -0,0 +1,9 @@
+import logo from './logo.png'
+import devLogo from './developer.svg'
+import developerFace from './developer-face.svg'
+
+export const assets = {
+  logo,
+  devLogo,
+  developerFace,
+}
